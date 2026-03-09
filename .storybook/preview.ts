74840a275947f3326d4241ab3603a0e1e2e67bdf
qcz <qicuizhi@yto.net.cn>
2026-03-09 17:55:50 +0800

    Screens

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,16 @@
 import type { Preview } from '@storybook/vue3-vite'
+import { setup } from '@storybook/vue3-vite'
+import { createPinia } from 'pinia'
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import '../src/index.css'
+
+//👇 Registers a global Pinia instance inside Storybook to be consumed by existing stories
+setup((app) => {
+  app.use(createPinia());
+});
+
+// Registers the msw addon
+initialize()
 const preview: Preview = {
   parameters: {
     controls: {
@@ -8,7 +19,8 @@ const preview: Preview = {
         date: /Date$/
       }
     }
-  }
+  },
+  loaders: [mswLoader],
 }
 
 export default preview
---
+++ src/components/InboxScreen.stories.ts
@@ -0,0 +1,50 @@
+
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+
+import { http, HttpResponse } from 'msw'
+
+import InboxScreen from './InboxScreen.vue'
+import { waitFor, waitForElementToBeRemoved } from 'storybook/test'
+import * as PureTaskListStories from './PureTaskList.stories.ts'
+
+const meta = {
+    component: InboxScreen,
+    title: 'InboxScreen',
+    tags: ['autodocs'],
+} satisfies Meta<typeof InboxScreen>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+                    return HttpResponse.json(PureTaskListStories.TaskListData);
+                }),
+            ],
+        },
+    },
+    play: async ({ canvas, userEvent }: any) => {
+        await waitForElementToBeRemoved(await canvas.findByTestId('empty'))
+        await waitFor(async () => {
+            await userEvent.click(canvas.getByLabelText('pinTask-1'))
+            await userEvent.click(canvas.getByLabelText('pinTask-3'))
+        })
+    },
+};
+
+export const Error: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+                http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+                    return new HttpResponse(null, {
+                        status: 403,
+                    });
+                }),
+            ],
+        },
+    },
+};
