dd17f8f3e6695018c4a2ba8a93569b29d3a6f4cd
qcz <qicuizhi@yto.net.cn>
2026-03-09 17:39:59 +0800

    Data

--- src/components/PureTaskList.stories.ts
+++ src/components/PureTaskList.stories.ts
@@ -1,7 +1,7 @@
 
 import type { Meta, StoryObj } from '@storybook/vue3-vite'
 
-import TaskList from './TaskList.vue'
+import PureTaskList from './PureTaskList.vue'
 
 import * as TaskStories from './Task.stories'
 
@@ -15,15 +15,15 @@ export const TaskListData = [
 ]
 
 const meta = {
-  component: TaskList,
-  title: 'TaskList',
+  component: PureTaskList,
+  title: 'PureTaskList',
   tags: ['autodocs'],
   excludeStories: /.*Data$/,
   decorators: [() => ({ template: '<div style="margin: 3em;"><story/></div>' })],
   args: {
     ...TaskStories.TaskData.events,
   },
-} satisfies Meta<typeof TaskList>
+} satisfies Meta<typeof PureTaskList>
 
 export default meta
 type Story = StoryObj<typeof meta>
