1ddcfe9ea8437cf3eb10e907917fcff0d5ca19e6
qcz <qicuizhi@yto.net.cn>
2026-03-10 10:38:06 +0800

    change task background to red2

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -18,7 +18,7 @@
         :id="'title-' + task.id"
         name="title"
         placeholder="Input title"
-+       style="background-color: red"
+        style="background-color: red"
       />
     </label>
     <button
